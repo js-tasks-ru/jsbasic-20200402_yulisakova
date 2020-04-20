b0b1fad0646f55fd18ca7d5c2861e41d8d98fcd5
javascript-ru <47786167+jsru@users.noreply.github.com>
2020-04-20 17:47:07 +0200

    Added task Функция showSalary(data,age)

---
+++ 3-module/4-task/index.js
@@ -0,0 +1,9 @@
+/**
+ * showSalary
+ * @param {Array} data - данные о пользователях
+ * @param {number} age - максимальный возраст
+ * @returns {string}
+ */
+function showSalary(data, age) {
+  // ваш код...
+}
