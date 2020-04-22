fa05539f067c61b87b174790b3cde730d55e2725
Yulia Isakova <yulisakova@gmail.com>
2020-04-22 19:39:20 +0300

    не решено, кроме 4-й

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,7 +1,20 @@
 /**
  * @param {string} str
  * @returns {string}
+ * преобразует строки вида 'my-short-string' в 'myShortString'.
  */
+"use strict";
+
 function camelize(str) {
-  // ваш код...
+  /* if (str[0] === "-") {
+    str = str.slice(1);
+  } */
+  str = str.split("-"); // разбиваю на массив 
+  
+  for (let i = 0; i < str.length; i++) { //заглавные буквы эл-тов массива
+    str[i] = str[i][0].toUpperCase() + str[i].slice(1); 
+  }
+
+  return str = str.join("");
+
 }
