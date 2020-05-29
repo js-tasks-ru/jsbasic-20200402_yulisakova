e7ea9f2d5c41f04e697dc5df21a778fdd841026f
Yulia Isakova <yulisakova@gmail.com>
2020-05-29 10:07:19 +0300

    3 модуль, задача 1,2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,10 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+     
+    let rowNumber = table.rows.length;
+
+    for (let i = 0; i < rowNumber; i++) {
+        table.rows[i].cells[i].style.backgroundColor = "red";
+    }
 }
